589cc936be88404a93fbd0c192aab9cae99801dd
Clement <jcsclement@users.noreply.github.com>
2018-08-20 10:21:16 +0530

    Update MountBlobStorePython.py

--- notebooks/epsilon-provisioning/MountBlobStorePython.py
+++ notebooks/epsilon-provisioning/MountBlobStorePython.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 StorageAccountName = "epsilonblobstore"
-StorageAccountAccessKey = "AE8bTgw6gMzktzeec6n+CtVD1j1Hzcq+gfQLqH+2LLpdRhfHxHRe03Q8zHxXxVHYZ+/OT+TJ8M30/NiPyCVBzA=="
+StorageAccountAccessKey = "###"
 StorageContainerName = "epsilon-container"
 StorageContainerSASKey = "?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2018-07-06T11:08:03Z&st=2018-02-06T03:08:03Z&spr=https&sig=4ROOYm1h32cZPcVi4s0XrgRVjWyHlBOcLB3PffC5%2BeI%3D"
 StorageFolderName = "mount-test03"
