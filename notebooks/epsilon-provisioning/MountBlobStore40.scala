60f64f38ca4513f8d52b53d47979ed2bb5219530
Clement <jcsclement@users.noreply.github.com>
2018-08-20 10:20:55 +0530

    Update MountBlobStore40.scala

--- notebooks/epsilon-provisioning/MountBlobStore40.scala
+++ notebooks/epsilon-provisioning/MountBlobStore40.scala
@@ -1,7 +1,7 @@
 // Databricks notebook source
 spark.conf.set(
   "fs.azure.account.key.epsilonblobstore.blob.core.windows.net",
-  "AE8bTgw6gMzktzeec6n+CtVD1j1Hzcq+gfQLqH+2LLpdRhfHxHRe03Q8zHxXxVHYZ+/OT+TJ8M30/NiPyCVBzA==")
+  "###")
 spark.conf.set(
   "fs.azure.sas.epsilon-container.epsilonblobstore.blob.core.windows.net",
   "?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2018-07-06T11:08:03Z&st=2018-02-06T03:08:03Z&spr=https&sig=4ROOYm1h32cZPcVi4s0XrgRVjWyHlBOcLB3PffC5%2BeI%3D")
